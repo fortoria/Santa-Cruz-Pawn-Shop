915ea0c36e644047ad3a49d976ad3200de66c9b7
root <root@li95-119.members.linode.com>
2012-01-26 18:49:30 +0000

    sup

--- server.js
+++ server.js
@@ -3,4 +3,6 @@ var app = express.createServer();
 
 app.use(express['static'](__dirname + '/www'));
 
-app.listen(process.env.PORT || 6000);
+app.listen(process.env.PORT || 8007);
+
+console.log(process.env.PORT || 8007);
