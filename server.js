766ca955f2254f9a1ce619447f1c7108ef05007c
root <root@li95-119.members.linode.com>
2012-01-26 19:45:16 +0000

    bltiz.io

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-#!/usr/bin/env node
-
 var express = require('express');
 var app = express.createServer();
 
 app.use(express['static'](__dirname + '/www'));
+
+app.get('/mu-3f11dd3c-7a3285d7-0d67644e-e1742723', function (req, res) {
+  res.send('42');
+});
 
 app.listen(process.env.PORT || 80);
