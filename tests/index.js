4fc5ae1af389b33e316b8926fca00bd12a294564
root <root@li95-119.members.linode.com>
2012-01-27 22:10:20 +0000

    change

--- tests/index.js
+++ tests/index.js
@@ -6,7 +6,7 @@ server.listen(process.env.PORT || 80);
 
 test("Test homepage HTTP", function (t) {
   t.plan(2);
-  request.get('http://localhost:' + (process.env.PORT || 80) + '/', function(err, res, data) {
+  request.get('http://localhost:' + (process.env.PORT || 80) + '/', function(err, res) {
     t.notOk(err, 'There should be no http errors');
     t.equal(res.statusCode, 200, 'status code should be 200');
     server.close();
