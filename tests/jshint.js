23cacf6ef7a57d00bf245bae8a6c0a86862d7b51
Henry Allen-Tilford <htilford@gmail.com>
2012-01-26 17:51:32 -0800

    added

--- tests/jshint.js
+++ tests/jshint.js
@@ -1,6 +1,6 @@
 var test = require("tap").test;
 console.log(__dirname)
-require('child_process').exec(__dirname + '/../node_modules/jshint/bin/hint ..', function (error, stdout, stderr) {
+require('child_process').exec(__dirname + '/../node_modules/.bin/jshint ..', function (error, stdout, stderr) {
     console.log('stdout: ' + stdout);
     console.log('stderr: ' + stderr);
     if (error !== null) {
